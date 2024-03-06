86a144af6578f315e051992386a374db4302a7b6
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-03-06 15:55:10 +0100

    Update script.js

--- 16/script.js
+++ 16/script.js
@@ -1,3 +1,9 @@
-document.querySelector('.burger').addEventListener('click', function() {
-    document.querySelector('.menu').classList.toggle('active');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
 });
