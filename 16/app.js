0dda01c90e031107e8b2894bf921dc51d88a29dd
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-03-07 12:49:52 +0100

    Update and rename app.js to script.js

--- 16/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.getElementById('menu-button');
-    const menu = document.getElementById('menu');
-
-    menuButton.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
-});
