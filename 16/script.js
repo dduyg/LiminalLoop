85f6cb3b3bd5c06a2e587010c28d4f5c23fed6b4
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-03-08 20:30:18 +0100

    Update script.js

--- 16/script.js
+++ 16/script.js
@@ -1,8 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuButton = document.getElementById('menuButton');
-  const floatingMenu = document.getElementById('floatingMenu');
+function convertToUpperCase() {
+  const inputText = document.getElementById('inputText').value;
+  const outputText = inputText.toUpperCase();
+  document.getElementById('outputText').value = outputText;
+}
 
-  menuButton.addEventListener('click', function () {
-    floatingMenu.classList.toggle('hidden');
-  });
-});
+function convertToLowerCase() {
+  const inputText = document.getElementById('inputText').value;
+  const outputText = inputText.toLowerCase();
+  document.getElementById('outputText').value = outputText;
+}
+
+function convertToCamelCase() {
+  const inputText = document.getElementById('inputText').value;
+  const words = inputText.split(' ');
+  const camelCaseText = words.map((word, index) => {
+    if (index === 0) {
+      return word.toLowerCase();
+    }
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }).join('');
+  document.getElementById('outputText').value = camelCaseText;
+}
+
+function convertToSnakeCase() {
+  const inputText = document.getElementById('inputText').value;
+  const snakeCaseText = inputText.toLowerCase().replace(/\s+/g, '_');
+  document.getElementById('outputText').value = snakeCaseText;
+}
